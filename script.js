220f0796b41be229c19ab6dde2d022fd707f66ab
JOÃO RAFAEL COSTA DO NASCIMENTO <joao.nascimento133@senaisp.edu.br>
2026-03-19 09:15:07 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const btn = document.getElementById("btn");
 const nome = document.getElementById("nome");
 const msg = document.getElementById("msg");
 
-btn.onclick = function () {
+btn.addEventListener = function () {
   msg.innerHTML = "Olá " + nome.value;
 };
